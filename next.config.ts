e7fd5efe648cfea76e56c9a0f77ab75e463517bf
Rajapolipilli <rajapolipilli@gmail.com>
2025-04-08 01:22:01 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,24 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)', // apply to all routes
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOWALL', // or remove this header entirely
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors *", // allow any site to embed your app
+          },
+        ],
+      },
+    ]
+  },
+
 };
 
 export default nextConfig;
