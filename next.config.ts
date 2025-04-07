c511d254b84a11a2e87a8ecb4ff385000584033d
Rajapolipilli <rajapolipilli@gmail.com>
2025-04-08 01:03:03 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pink-junior-peacock-757.mypinata.cloud',
+        pathname: '/ipfs/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
